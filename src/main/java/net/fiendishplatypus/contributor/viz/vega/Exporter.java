d835f81148b0c8ff3a2582eb0334806f2e6e66e2
aardvark <aardvark@bk.ru>
2024-04-21 18:54:05 +0200

    Add logic to anonymize contributors id on export to the json

--- src/main/java/net/fiendishplatypus/contributor/viz/vega/Exporter.java
+++ src/main/java/net/fiendishplatypus/contributor/viz/vega/Exporter.java
@@ -4,15 +4,34 @@ import net.fiendishplatypus.contributor.Contributor;
 import net.fiendishplatypus.contributor.Period;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringJoiner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Exporter {
+  /**
+   * When sets to true generate data with contributor ids anonimized.
+   */
+  private final boolean anonymize;
+
+  public Exporter() {
+    this.anonymize = true;
+  }
+
+  public Exporter(boolean anonymize) {
+    this.anonymize = anonymize;
+  }
+
 
   public String toDataSection(Collection<Contributor> xs) {
+    Map<String, String> anonymousIds = new HashMap<>();
+    AtomicInteger i = new AtomicInteger(0);
+
     StringJoiner sj = new StringJoiner(",\n");
     for (Contributor x : xs) {
-      String id = x.getId();
+      String id = anonymize ? anonymousIds.computeIfAbsent(x.getId(), __ -> "#" + i.incrementAndGet()) : x.getId();
       List<Period> periods = x.getPeriods();
       for (Period period : periods) {
         //@formatter:off
